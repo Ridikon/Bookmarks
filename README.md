7e11076cec4c2d6cb75aa64dac06cdf8b154269a
Vitalii <ridikon666@gmail.com>
2018-07-19 00:15:21 +0300

    Start implements settings layout

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ProjektX
+# ProjectX
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDGwvHlqT8mjmdf7SxGXcSkFE-dfoT7fd8",
+    authDomain: "bookmarksproject-30298.firebaseapp.com",
+    databaseURL: "https://bookmarksproject-30298.firebaseio.com",
+    projectId: "bookmarksproject-30298",
+    storageBucket: "bookmarksproject-30298.appspot.com",
+    messagingSenderId: "762071137913"
+  }
 };
